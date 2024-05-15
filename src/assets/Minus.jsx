a17096a247dec2018b7e733f3e08080b4865506e
Wargen Guittap <wargenng@gmail.com>
2024-05-15 14:21:26 -0700

    refactoring svg's

---
+++ src/assets/Minus.jsx
@@ -0,0 +1,15 @@
+export default function Minus() {
+    return (
+        <svg
+            fill="currentColor"
+            stroke-width="0"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            height="3rem"
+            width="3rem"
+            style="overflow: visible; color: currentcolor;"
+        >
+            <path d="M64 32C28.7 32 0 60.7 0 96v320c0 35.3 28.7 64 64 64h320c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm88 200h144c13.3 0 24 10.7 24 24s-10.7 24-24 24H152c-13.3 0-24-10.7-24-24s10.7-24 24-24z"></path>
+        </svg>
+    );
+}
